9eb4fb936174af6a2af87d339996d64242dd5a3e
Rodrigo Cruz <rodrigo.sites@gmail.com>
2014-10-21 12:37:08 -0200

    Primeiro commit
    Aplicação zerada - rails new -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Conservatoriohvl::Application.config.secret_key_base = 'cb79be58f3433290137785abc05bb78620b1399a2e81f0f0c0375a52fa42c7fe0c84b684e7b7483c63c60aff16205e3dec5cac2f266387f218cfd32198d5a89d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Conservatoriohvl::Application.config.session_store :cookie_store, key: '_conservatoriohvl_session'
