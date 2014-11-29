32dd682e8916b460acbe55443b6b4ed807b27126
Rodrigo Cruz <rodrigo.sites@gmail.com>
2014-11-29 20:26:13 -0200

    Último commit facinho....
    Agora começa a lógica complexa do módulo Administrativo.
    
    Apenas por precaução... :+1:

--- app/helpers/clientes_helper.rb
+++ app/helpers/clientes_helper.rb
@@ -1,2 +1,8 @@
 module ClientesHelper
+
+  # Método para acessar o banco pela View (não quebrar a regra MVC de acesso a base pela View)
+  def get_clientes (idcliente)
+      @horarios = Aluno.where("idcliente = #{idcliente}")
+  end
+
 end
--- app/helpers/salas_helper.rb
+++ app/helpers/salas_helper.rb
@@ -1,2 +1,8 @@
 module SalasHelper
+  
+  # Método para acessar o banco pela View (não quebrar a regra MVC de acesso a base pela View)
+  def get_horarios (idsala)
+      @horarios = Horario.where("idsala = #{idsala}").order('horario')
+  end
+
 end
