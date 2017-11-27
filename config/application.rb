06ddb7eb78c84d9b6c40183f7bbeeec1ed7074b5
Rodrigo Cruz <rodrigo.sites@gmail.com>
2017-11-27 20:26:49 -0200

    vendor assets em production 2

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Conservatoriohvl
     # config.i18n.default_locale = :de
 
     config.serve_static_assets = true
+    config.assets.serve_static_files = true
     config.i18n.default_locale = :"pt-BR"
     I18n.enforce_available_locales = false
   end
